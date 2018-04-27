1504ccb03ac03fc7608a0b50fd9fe06419fd13da
afeiship <1290657123@qq.com>
2018-04-28 00:25:27 +0800

    issue: tar npm

--- index.sh
+++ index.sh
@@ -3,3 +3,7 @@
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
 alias tar-all='for file in *.tgz; do tar -zxf $file; done';
+
+## zip && unzip
+# alias tar-gz='tar zcf $1.tar.gz ';
+# alias tar-ugz='tar *.tar.gz';
