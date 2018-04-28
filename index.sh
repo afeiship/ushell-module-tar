ea36142bc7c0ade566a611f857be500a404ecad0
afeiship <1290657123@qq.com>
2018-04-28 12:12:27 +0800

    issue: add tar-gz/ugz

--- index.sh
+++ index.sh
@@ -5,5 +5,5 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 alias tar-all='for file in *.tgz; do tar -zxf $file; done';
 
 ## zip && unzip
-# alias tar-gz='tar zcf $1.tar.gz ';
-# alias tar-ugz='tar *.tar.gz';
+alias tar-gz="tar zcf $1.tar.gz $1";
+alias tar-ugz='tar *.tar.gz';
