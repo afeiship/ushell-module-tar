34edb9ea2f0d00c2e50705da2d62638f8c62b4bf
afeiship <1290657123@qq.com>
2021-11-15 17:27:05 +0800

    feat: update tarz/tarx

--- index.sh
+++ index.sh
@@ -4,6 +4,8 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 
 alias tar-all='for file in *.tgz; do tar -zxf $file; done';
 
-## tar gz/ugz
-alias tar-gz="${ROOT_PATH}/tar-gz.sh";
+## tar tarz/tarx/ugz
+alias tarz="${ROOT_PATH}/modules/tar-gz.sh";
+alias tarx="${ROOT_PATH}/modules/tar-xz.sh";
+
 alias tar-ugz='tar *.tar.gz';
--- tar-gz.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-## zip && unzip
-alias tar-ugz='tar *.tar.gz';
-
-#!/usr/bin/env bash
-tar zcf $1.tar.gz $1
