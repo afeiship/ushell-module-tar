f728874af22719bf957d9dada420753efe5a09f8
afeiship <1290657123@qq.com>
2022-02-24 09:01:13 +0800

    feat: tarz -> tz; tarx -> tx

--- index.sh
+++ index.sh
@@ -5,7 +5,7 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 alias tar-all='for file in *.tgz; do tar -zxf $file; done';
 
 ## tar tarz/tarx/ugz
-alias tarz="${ROOT_PATH}/modules/tar-gz.sh";
-alias tarx="${ROOT_PATH}/modules/tar-xz.sh";
+alias tz="${ROOT_PATH}/modules/tar-gz.sh";
+alias tx="${ROOT_PATH}/modules/tar-xz.sh";
 
 alias tar-ugz='tar *.tar.gz';
